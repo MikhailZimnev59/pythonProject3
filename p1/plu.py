14448af9d7a31c9a39442b88b253cbca73dcbc51
Mikhail_Z <m.zimnev@yandex.ru>
2023-01-15 18:17:54 +0300

    function multi is added

--- p1/plu.py
+++ p1/plu.py
@@ -1,7 +1,8 @@
 def plus(a, b):
     return a + b
-def minus(a, b):
-    return a - b
+
+def multi(a, b):
+    return a * b
 if __name__ == "__main__":
     print(plus(2, 3))
-    print(minus(5, 2))
+    print(multi(5, 2))
